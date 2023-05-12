5f377232dcde1de9bb37be78eea0ab6056951f90
Geert Verstijnen <geertverstijnen@bycape.io>
2023-05-12 14:05:13 +0200

    setup

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(Buildcontext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My App'),
+      ),
+      body: const Center(
+        child: Text('Welcome to my app'),
+        body
+      ),
+    );
+  }
+}
