3ff41c32ddb02b820e4663cddd7daeef1527f4ec
Geert Verstijnen <81244861+GeertVerstijnen@users.noreply.github.com>
2023-05-22 14:42:43 +0200

    update font + question screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nieuw/screens/home_screen.dart';
 import 'package:nieuw/screens/identification_screen.dart';
+import 'package:nieuw/screens/question_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -20,7 +21,7 @@ class _SplashScreenState extends State<SplashScreen>
     
     Future.delayed(Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (_) => IdentificationScreen(),
+          builder: (_) => QuestionScreen(),
         ),
       );
     });
