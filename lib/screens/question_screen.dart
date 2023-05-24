0c5b33e0a47c1b4576b0534e49a0c6549e0e32ea
Geert Verstijnen <81244861+GeertVerstijnen@users.noreply.github.com>
2023-05-24 14:18:03 +0200

    updatjes

--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nieuw/main.dart';
-
 import '../widgets/background.dart';
 
 class QuestionScreen extends StatefulWidget {
@@ -9,23 +11,111 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
-  int elapsedTime = 0;
   late DateTime startTime;
+  late Timer timer;
+  String formattedTime = '00:00';
+  List<String> code = List.generate(6, (_) => '');
+  List<FocusNode> focusNodes = List.generate(6, (_) => FocusNode());
 
   @override
   void initState() {
     super.initState();
     startTime = DateTime.now();
     startTimer();
+    focusNodes[0].requestFocus();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    for (var node in focusNodes) {
+      node.dispose();
+    }
+    super.dispose();
   }
 
   void startTimer() {
-    Future.delayed(const Duration(seconds: 1), () {
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
-        elapsedTime = DateTime.now().difference(startTime).inSeconds;
+        Duration timeElapsed = DateTime.now().difference(startTime);
+        formattedTime = formatDuration(timeElapsed);
       });
-      startTimer();
     });
+  }
+
+  String formatDuration(Duration duration) {
+    String twoDigits(int n) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String minutes = twoDigits(duration.inMinutes);
+    String seconds = twoDigits(duration.inSeconds.remainder(60));
+    return '$minutes:$seconds';
+  }
+
+  void handleKeyPress(RawKeyEvent event, int index) {
+    if (event is RawKeyDownEvent) {
+      if (event.logicalKey == LogicalKeyboardKey.backspace) {
+        if (index > 0) {
+          setState(() {
+            focusNodes[index - 1].requestFocus();
+            code[index - 1] = '';
+          });
+        }
+      } else if (event.logicalKey == LogicalKeyboardKey.enter ||
+          event.logicalKey == LogicalKeyboardKey.space) {
+        if (index < 5) {
+          setState(() {
+            focusNodes[index + 1].requestFocus();
+          });
+        }
+      } else {
+        String keyPressed = event.character?.toUpperCase() ?? '';
+        if (keyPressed.isNotEmpty) {
+          setState(() {
+            code[index] = keyPressed;
+          });
+          if (index < 5) {
+            setState(() {
+              focusNodes[index + 1].requestFocus();
+            });
+          }
+        }
+      }
+    }
+  }
+
+  bool isCodeCorrect() {
+    String enteredCode = code.join('');
+    String hardcodedCode = 'ABCDEF'; // Hardcoded code
+    return enteredCode.toLowerCase() == hardcodedCode.toLowerCase();
+  }
+
+  void submitCode() {
+    if (isCodeCorrect()) {
+      // Code is correct, perform desired action
+      print('Code is correct');
+    } else {
+      // Code is incorrect, display error message
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Foutieve code'),
+            content: Text('De ingevoerde code is incorrect. Probeer het opnieuw.'),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
@@ -47,6 +137,40 @@ class _QuestionScreenState extends State<QuestionScreen> {
                   ),
                 ),
               ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(6, (index) {
+                  return SizedBox(
+                    width: 40,
+                    child: RawKeyboardListener(
+                      focusNode: focusNodes[index],
+                      onKey: (event) => handleKeyPress(event, index),
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                        ),
+                        child: Text(
+                          code[index],
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: submitCode,
+                  child: Text('Selecteer Code'),
+                ),
+              ),
               const Spacer(),
               Padding(
                 padding: const EdgeInsets.only(bottom: 42),
@@ -55,23 +179,20 @@ class _QuestionScreenState extends State<QuestionScreen> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      const SizedBox(height: 20),
                       Text(
                         'Huidige tijd:',
                         style: TextStyle(
                           fontSize: 24,
                           color: Colors.white,
                           fontWeight: FontWeight.bold,
-                          fontFamily: 'Nunito',
                         ),
                       ),
                       Text(
-                        '$elapsedTime seconden',
+                        formattedTime,
                         style: TextStyle(
                           fontSize: 16,
                           color: Colors.white,
                           fontWeight: FontWeight.normal,
-                          fontFamily: 'Nunito',
                         ),
                       ),
                     ],
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:nieuw/screens/code_screen.dart';
 import 'package:nieuw/screens/home_screen.dart';
 import 'package:nieuw/screens/identification_screen.dart';
 import 'package:nieuw/screens/question_screen.dart';
